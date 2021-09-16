cfaa8afb23496c2e9218eb990d494e0e89ca9509
Jessica Nelson <yahwehschica@yahoo.com>
2021-09-16 11:23:07 -0400

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+const thankYouNotes = ["Anna", "Bob", "Cathy"];
+
+function writeCards(thankYouNotes) {
+    for (let i =0; i < thankYouNotes.length; i++) {
+    console.log(`Thank you ${thankYouNotes[i]} for the awesome gift!`);
+}
+return thankYouNotes;
+}
+
+writeCards(thankYouNotes);
+
+//=> while loop
+
+
+
+function countDown() {
+    let countDown = 18; 
+    while (countDown > -1) {
+        console.log (countDown--);
+        }
+        return countDown
+    }
+countDown();
+
